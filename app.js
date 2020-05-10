69be989a215cc9fdb3c34109bcc7bdddb44fbccd
hvan307 <hongvan307@gmail.com>
2020-05-10 01:18:02 +0100

    working and styled navbar menu with toggle

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ function main() {
   const topNav = document.querySelector('.top-nav')
 
   menuToggler.addEventListener('click', () => {
-    console.log('hello')
     menuToggler.classList.toggle('open')
     topNav.classList.toggle('open')
   })
 }
 
 window.addEventListener('DOMContentLoaded', main)
-
-// $(document).ready(function () {
-//   $('.menu-toggler').on('click', function () {
-//     $(this).toggleClass('open')
-//     $('.top-nav').toggleClass('open')
