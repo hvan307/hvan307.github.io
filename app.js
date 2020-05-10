48145b55344da81adabf117d533968f575f32404
hvan307 <hongvan307@gmail.com>
2020-05-10 21:54:13 +0100

    name animation hero

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 function main() {
   const menuToggler = document.querySelector('.menu-toggler')
   const topNav = document.querySelector('.top-nav')
+  const modalButton = document.querySelector('.modal-button')
+  const modal = document.querySelector('.modal')
+  const closeButton = document.querySelector('.close')
 
   menuToggler.addEventListener('click', () => {
     menuToggler.classList.toggle('open')
     topNav.classList.toggle('open')
+  })
+
+  modalButton.addEventListener('click', () => {
+    modal.classList.toggle('is-active')
+  })
+
+  closeButton.addEventListener('click', () => {
+    modal.classList.toggle('is-active')
   })
 }
 
