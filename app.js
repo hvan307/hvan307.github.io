8d5139a687613a596a0886ee5c70637df3eda848
hvan307 <hongvan307@gmail.com>
2020-05-11 15:08:30 +0100

    fixed top-nav links

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 function main() {
   const menuToggler = document.querySelector('.menu-toggler')
   const topNav = document.querySelector('.top-nav')
-  const navLink = document.querySelector('.nav-link')
+  const navLinkHome = document.querySelector('.nav-link.home')
+  const navLinkAbout = document.querySelector('.nav-link.about')
+  const navLinkSkills = document.querySelector('.nav-link.skills')
+  const navLinkPortfolio = document.querySelector('.nav-link.portfolio')
+  const navLinkContacts = document.querySelector('.nav-link.contacts')
   const modalButton = document.querySelector('.modal-button')
   const modal = document.querySelector('.modal')
   const closeButton = document.querySelector('.modal-close')
@@ -11,7 +15,27 @@ function main() {
     topNav.classList.toggle('open')
   })
 
-  navLink.addEventListener('click', () => {
+  navLinkHome.addEventListener('click', () => {
+    menuToggler.classList.remove('open')
+    topNav.classList.remove('open')
+  })
+
+  navLinkAbout.addEventListener('click', () => {
+    menuToggler.classList.remove('open')
+    topNav.classList.remove('open')
+  })
+
+  navLinkSkills.addEventListener('click', () => {
+    menuToggler.classList.remove('open')
+    topNav.classList.remove('open')
+  })
+
+  navLinkPortfolio.addEventListener('click', () => {
+    menuToggler.classList.remove('open')
+    topNav.classList.remove('open')
+  })
+
+  navLinkContacts.addEventListener('click', () => {
     menuToggler.classList.remove('open')
     topNav.classList.remove('open')
   })
